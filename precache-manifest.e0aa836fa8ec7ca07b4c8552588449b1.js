702e7fc11a9ae3e48bd99f838daecd9f444e0c64
gooff1997 <gooff1997@outlook.com>
2020-01-21 17:57:50 +0800

    deploy

---
+++ precache-manifest.e0aa836fa8ec7ca07b4c8552588449b1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21ec65ef775fae3dc2b82ad19487c31c",
+    "url": "/react-antd-admin/index.html"
+  },
+  {
+    "revision": "df9b65e551fe0b51e6a7",
+    "url": "/react-antd-admin/static/css/2.35f98baf.chunk.css"
+  },
+  {
+    "revision": "85c8cacf8c782c3984a4",
+    "url": "/react-antd-admin/static/css/main.6e90ce9b.chunk.css"
+  },
+  {
+    "revision": "df9b65e551fe0b51e6a7",
+    "url": "/react-antd-admin/static/js/2.03ab00b2.chunk.js"
+  },
+  {
+    "revision": "e23fabadb975545b74e4a8a97216a709",
+    "url": "/react-antd-admin/static/js/2.03ab00b2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "85c8cacf8c782c3984a4",
+    "url": "/react-antd-admin/static/js/main.10b4bbfe.chunk.js"
+  },
+  {
+    "revision": "37741c9c8781d9f11f76",
+    "url": "/react-antd-admin/static/js/runtime-main.42ac6db7.js"
+  }
+]);
